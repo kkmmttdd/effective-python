bef4de8fdfde8d75cd62565699760dbb3372edff
kkmmttdd <kkmmttdd@gmail.com>
2020-11-16 16:00:09 +0900

    star assign

---
+++ 02_lists_and_dictionaries/13_prefer_catch_all/main.py
@@ -0,0 +1,6 @@
+car_ages = [0, 9, 4, 8, 7, 20, 19, 1, 6, 15]
+car_ages_descending = sorted(car_ages, reverse=True)
+# oldest, second_oldest = car_ages_descending
+oldest, second_oldest, *others = car_ages_descending
+
+print(oldest, second_oldest, others)
