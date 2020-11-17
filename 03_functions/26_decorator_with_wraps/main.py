b63affeb70fe6e4e05c2554baf2bda9a5f94c12b
kkmmttdd <kkmmttdd@gmail.com>
2020-11-17 14:50:22 +0900

    simple wrapper (userless)

---
+++ 03_functions/26_decorator_with_wraps/main.py
@@ -0,0 +1,21 @@
+def trace(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print(f'{func.__name__} ({args!r}, {kwargs!r})'
+              f'-> {result!r}'
+              )
+        return result
+    return wrapper
+
+
+#  equivalent to trace(fibonacci)
+@trace
+def fibonacci(n):
+    if n in (0, 1):
+        return n
+    return fibonacci(n - 2) + fibonacci(n - 1)
+
+
+fibonacci(10)
+
+print(fibonacci)
