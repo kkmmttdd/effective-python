400004b9ad0bbf2eecbc6afdb5152e47386d79bb
kkmmttdd <kkmmttdd@gmail.com>
2020-11-17 14:51:32 +0900

    @wraps

--- 03_functions/26_decorator_with_wraps/main.py
+++ 03_functions/26_decorator_with_wraps/main.py
@@ -1,4 +1,8 @@
+from functools import wraps
+
+
 def trace(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)
         print(f'{func.__name__} ({args!r}, {kwargs!r})'
