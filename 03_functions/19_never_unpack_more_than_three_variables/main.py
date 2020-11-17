2dbe1d64555e871f27488ef738be60d41861c1de
kkmmttdd <kkmmttdd@gmail.com>
2020-11-17 10:00:03 +0900

    unpack

---
+++ 03_functions/19_never_unpack_more_than_three_variables/main.py
@@ -0,0 +1,34 @@
+def get_status(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    return minimum, maximum
+
+
+lengths = [63, 73, 72, 60, 67, 66, 71, 61, 72, 70]
+minimum, maximum = get_status(lengths)
+print(f'Min: {minimum}, Max: {maximum}')
+
+first, second = 1, 2
+
+
+def my_function():
+    return 1, 2
+
+
+first, second = my_function()
+
+
+def get_avg_ratio(numbers):
+    average = sum(numbers) / len(numbers)
+    scaled = [x / average for x in numbers]
+    scaled.sort(reverse=True)
+    return scaled
+
+
+longest, *middle, shortest = get_avg_ratio(lengths)
+print(f'Longest: {longest:>4.0%}')
+print(f'Shortest: {shortest:>4.0%}')
+
+
+assert first == 1
+assert second == 2
